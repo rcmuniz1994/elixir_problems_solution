6a314c4ca9aadb78e8f3d7130104b10f300ff336
Rodrigo <rodrigo.muniz@evolux.net.br>
2020-04-07 23:40:41 -0300

    first solution

---
+++ challenge1.exs
@@ -0,0 +1,12 @@
+defmodule Cha1 do
+  def diff_set(input_list, k) do
+    List.foldr(input_list, [], fn item, acc -> acc++[k-item] end)
+    |> MapSet.new()
+  end
+
+  def list2Set(input_list), do: input_list |> MapSet.new()
+
+  def match_sum(input_list, k) do
+    !MapSet.disjoint?(diff_set(input_list, k), list2Set(input_list))
+  end
+end
---
+++ mean.exs
@@ -0,0 +1,8 @@
+defmodule Mean do
+  def mean(score1, score2) do
+    mean = (score1 + score2)/2
+    "Your mean was #{mean}"
+  end
+end
+
+#  numbers = %{"zero" => 0, "one" => 1, "two"=> 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, "nine"=> 9}
